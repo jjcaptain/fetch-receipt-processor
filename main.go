e3836b95cb958f27d9dddb888bb90b3099fb2702
Jeff Captain <jjcaptain@gmail.com>
2023-05-14 13:21:57 -0500

    Add REST API dependency
    The app now runs a web server on port 8080 and has a hello world endpoint.

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	router := gin.Default()
+
+	router.GET("/hello", hello)
+
+	router.Run("localhost:8080")
+}
+
+func hello(context *gin.Context) {
+	context.JSON(http.StatusOK, "Hello World")
 }
