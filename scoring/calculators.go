bb4005758aad7476c83bf81ecadb5b2a3749169a
Jeff Captain <jjcaptain@gmail.com>
2023-05-16 21:26:08 -0500

    Add scoring functions
    Added the functions that calculate the score for each part of the receipt.

---
+++ scoring/calculators.go
@@ -0,0 +1,127 @@
+package scoring
+
+import (
+	"jjcaptain/receipt-processor/types"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Calculate points for the retailer
+func ScoreRetailer(receipt types.Receipt) int {
+	// Award one point per alphanumeric character
+	nonAlphanumericRegex := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+	filteredRetailerName := nonAlphanumericRegex.ReplaceAllString(receipt.Retailer, "")
+	return len(filteredRetailerName)
+}
+
+// Calculate points for the receipt total
+func ScoreTotal(receipt types.Receipt) (int, error) {
+	// First make sure it's a number
+	total, err := strconv.ParseFloat(receipt.Total, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	// Multiply by 100 and convert to int to make the comparisons cleaner
+	boostedTotal := int(total * 100)
+
+	score := 0
+	// 50 points if the total is a round dollar amount with no cents
+	if boostedTotal % 100 == 0 {
+		score += 50
+	}
+	// 25 points if the total is a multiple of 0.25
+	if boostedTotal % 25 == 0 {
+		score += 25
+	}
+	
+	return score, nil
+}
+
+// Calculate points for items on the receipt
+func ScoreItems(receipt types.Receipt) (int, error) {
+	score := 0
+
+	numItems := len(receipt.Items)
+	// 5 points for every two items on the receipt
+	score += (5 * (numItems / 2))
+
+	ch := make(chan int, numItems)
+	errs := make(chan error, 1)
+
+	// calculate the score for each item
+	for i := 0; i < numItems; i++ {
+		go scoreItem(receipt.Items[i], ch, errs)
+	}
+
+	// add up the results
+	var itemScore int
+	var err error
+	for i := 0; i < numItems; i++ {
+		select {
+		case itemScore = <- ch:
+			score += itemScore
+		case err = <- errs:
+			return 0, err
+		}
+	}
+
+	return score, nil
+}
+
+// Calculate points for a single item on the receipt
+func scoreItem(item types.ReceiptItem, ch chan int, errs chan error) {
+	score := 0
+
+	// If the trimmed length of the item description is a multiple of 3
+	trimmedDescription := strings.TrimSpace(item.ShortDescription)
+	if len(trimmedDescription) % 3 == 0 {
+		price, err := strconv.ParseFloat(item.Price, 32)
+		if err != nil {
+			errs <- err
+		}
+
+		// multiply the price by 0.2 and round up to the nearest integer
+		score = int(math.Ceil(price * 0.2))
+	}
+
+	ch <- score
+}
+
+// Calculate points for the receipt date
+func ScoreDate(receipt types.Receipt) (int, error) {
+	purchaseDate, err := time.Parse(time.DateOnly, receipt.PurchaseDate)
+	if err != nil {
+		return 0, err
+	}
+
+	score := 0
+	// 6 points if the day in the purchase date is odd
+	if purchaseDate.Day() % 2 == 1 {
+		score = 6
+	}
+
+	return score, nil
+}
+
+// Calculate points for the receipt time
+func ScoreTime(receipt types.Receipt) (int, error) {
+	purchaseTime, err := time.Parse("15:04", receipt.PurchaseTime)
+	if err != nil {
+		return 0, err
+	}
+
+	score := 0
+	// 10 points if the time of purchase is after 2:00pm and before 4:00pm
+	startTime := time.Date(purchaseTime.Year(), purchaseTime.Month(), purchaseTime.Day(), 14, 0, 0, 0, purchaseTime.Location())
+	endTime := time.Date(purchaseTime.Year(), purchaseTime.Month(), purchaseTime.Day(), 16, 0, 0, 0, purchaseTime.Location())
+
+	if purchaseTime.After(startTime) && purchaseTime.Before(endTime) {
+		score = 10
+	}
+
+	return score, nil
+}
---
+++ scoring/calculators_test.go
@@ -0,0 +1,268 @@
+package scoring
+
+import (
+	"jjcaptain/receipt-processor/types"
+	"testing"
+)
+
+func TestScoreRetailer_SimpleCase(t *testing.T) {
+	input := types.Receipt{Retailer: "Target"}
+	expected := 6
+
+	score := ScoreRetailer(input)
+
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreRetailer_WithSymbols(t *testing.T) {
+	input := types.Receipt{Retailer: "M&M Corner Market"}
+	expected := 14
+
+	score := ScoreRetailer(input)
+
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreTotal_ValueWithNondivisibleCents(t *testing.T) {
+	input := types.Receipt{Total: "35.35"}
+	expected := 0
+
+	score, err := ScoreTotal(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreTotal_ValueWithDivisibleCents(t *testing.T) {
+	input := types.Receipt{Total: "22.25"}
+	expected := 25
+
+	score, err := ScoreTotal(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreTotal_ValueWithWholeDollar(t *testing.T) {
+	input := types.Receipt{Total: "12.00"}
+	expected := 75
+
+	score, err := ScoreTotal(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreTotal_InvalidValue(t *testing.T) {
+	input := types.Receipt{Total: "notAnumber"}
+
+	_, err := ScoreTotal(input)
+
+	if err == nil {
+		t.Error("Expected error but did not get one")
+	}
+}
+
+func TestScoreItems_EvenItemCount(t *testing.T) {
+	input := types.Receipt{Items: []types.ReceiptItem{
+		{ShortDescription: "Gatorade", Price: "2.25"},
+		{ShortDescription: "Gatorade", Price: "2.25"},
+		{ShortDescription: "Gatorade", Price: "2.25"},
+		{ShortDescription: "Gatorade", Price: "2.25"}} }
+	expected := 10
+
+	score, err := ScoreItems(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreItems_OddItemCount(t *testing.T) {
+	input := types.Receipt{Items: []types.ReceiptItem{
+		{ShortDescription: "Gatorade", Price: "2.25"},
+		{ShortDescription: "Gatorade", Price: "2.25"},
+		{ShortDescription: "Gatorade", Price: "2.25"},
+		{ShortDescription: "Gatorade", Price: "2.25"},
+		{ShortDescription: "Gatorade", Price: "2.25"}} }
+	expected := 10
+
+	score, err := ScoreItems(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreItems_ScorableDescription(t *testing.T) {
+	input := types.Receipt{Items: []types.ReceiptItem{
+		{ShortDescription: "Emils Cheese Pizza", Price: "12.25"}} }
+	expected := 3
+
+	score, err := ScoreItems(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreItems_DescriptionWithExtraSpaces(t *testing.T) {
+	input := types.Receipt{Items: []types.ReceiptItem{
+		{ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ", Price: "12.00"}} }
+	expected := 3
+
+	score, err := ScoreItems(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreItems_MeetsMultipleCriteria(t *testing.T) {
+	input := types.Receipt{Items: []types.ReceiptItem{
+		{ShortDescription: "Mountain Dew 12PK", Price: "6.49"},
+		{ShortDescription: "Emils Cheese Pizza", Price: "12.25"},
+		{ShortDescription: "Knorr Creamy Chicken", Price: "1.26"},
+		{ShortDescription: "Doritos Nacho Cheese", Price: "3.35"},
+		{ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ", Price: "12.00"}} }
+
+	expected := 16
+
+	score, err := ScoreItems(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreItems_PriceNotANumber(t *testing.T) {
+	input := types.Receipt{Items: []types.ReceiptItem{
+		{ShortDescription: "Emils Cheese Pizza", Price: "pizza"}} }
+
+	_, err := ScoreItems(input)
+
+	if err == nil {
+		t.Error("Expected error but did not get one")
+	}
+}
+
+func TestScoreDate_OddDay(t *testing.T) {
+	input := types.Receipt{PurchaseDate: "2023-05-15"}
+	expected := 6
+
+	score, err := ScoreDate(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreDate_EvenDay(t *testing.T) {
+	input := types.Receipt{PurchaseDate: "2023-05-16"}
+	expected := 0
+
+	score, err := ScoreDate(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreDate_InvalidDay(t *testing.T) {
+	input := types.Receipt{PurchaseDate: "notAdate"}
+
+	_, err := ScoreDate(input)
+
+	if err == nil {
+		t.Error("Expected error but did not get one")
+	}
+}
+
+func TestScoreTime_TimeTooEarly(t *testing.T) {
+	input := types.Receipt{PurchaseTime: "13:01"}
+	expected := 0
+
+	score, err := ScoreTime(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreTime_TimeTooLate(t *testing.T) {
+	input := types.Receipt{PurchaseTime: "17:01"}
+	expected := 0
+
+	score, err := ScoreTime(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreTime_TimeInRange(t *testing.T) {
+	input := types.Receipt{PurchaseTime: "14:33"}
+	expected := 10
+
+	score, err := ScoreTime(input)
+
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+	if score != expected {
+		t.Errorf("Score %d did not match expected %d", score, expected)
+	}
+}
+
+func TestScoreTime_InvalidTime(t *testing.T) {
+	input := types.Receipt{PurchaseTime: "notAtime"}
+
+	_, err := ScoreTime(input)
+
+	if err == nil {
+		t.Error("Expected error but did not get one")
+	}
+}
